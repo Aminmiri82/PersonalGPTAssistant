fdd2a2c1ddadb19c36ed61d787b6b96731daf408
Aminmiri82 <miri.amin@ymail.com>
2024-02-16 18:13:34 +0100

    bug fixes and improvements

--- SRC/App.js
+++ SRC/App.js
@@ -5,21 +5,36 @@ import Screen from './Components/Screen';
 import NavBar from './Components/NavBar';
 
 
+
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Header from './Components/Header';
 import colors from './config/colors';
 import AppText from './Components/AppText';
 import AppTextInput from './Components/AppTextInput';
+import AssistantsMenuItem from './Components/AssistantsMenuItem';
+import ListItem from './Components/ListItem';
+import ListItemDeleteAction from './Components/ListItemDeleteAction';
 export default function App() {
   
   return (
     <>
     <Header title="test title" backButton="" />
-    <Screen>
+    <Screen style={styles.container}>
+      <AssistantsMenuItem image={require('./assets/mosh.jpg')} modelName="Assistant 1" />
+      <AssistantsMenuItem image={require('./assets/mosh.jpg')} modelName="Assistant 2" />
+      <ListItem
+        modelName="My title"
+        chatName="Chat name"
+        chatMessage="Chat message"
+        image={require('./assets/mosh.jpg')}
+        renderRightActions={ListItemDeleteAction}
+        />
+
+
       
     
     
-  <AppTextInput placeholder="Ask me anything" />
+  
       
       
       
@@ -36,9 +51,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: 'space-between', // This will space out the items evenly
+    alignItems: 'flex-start',
+    maxWidth: '100%', // Ensuring the container doesn't stretch too much
   },
 });
