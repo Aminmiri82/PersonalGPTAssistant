d82a64dc1bf3eea62c2c69a1f4a121351d3e2d77
Aminmiri82 <miri.amin@ymail.com>
2024-02-19 16:40:58 +0100

    added ScrollableAppText and terms and conditions screen

--- SRC/App.js
+++ SRC/App.js
@@ -1,39 +1,36 @@
-
-import { StyleSheet, Text, View } from 'react-native';
-
-import Screen from './Components/Screen';
+import React from 'react';
+import { StyleSheet, Text, View,ScrollView } from 'react-native';
 import NavBar from './Components/NavBar';
-
-
-
-import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Header from './Components/Header';
-import colors from './config/colors';
+import Screen from './Components/Screen';
+import TermsAndConditionsScreen from './Screens/TermsAndConditionsScreen';
 import AppText from './Components/AppText';
-import AppTextInput from './Components/AppTextInput';
-import AssistantsMenuItem from './Components/AssistantsMenuItem';
-import ListItem from './Components/ListItem';
-import ListItemDeleteAction from './Components/ListItemDeleteAction';
+import ScrollableAppText from './Components/ScrollableAppText';
+
+
+
+
 export default function App() {
   
   return (
     <>
-    <Header title="test title" backButton="" />
-    <Screen style={styles.container}>
-    <ListItem
-    modelName="something"
-    chatMessage="hi"
-    chatName="thing"
-    image={require('./assets/mosh.jpg')}
-    renderRightActions={ListItemDeleteAction}
-    />
+    <Header title="test"/>
+    <Screen>
+    <TermsAndConditionsScreen/>
+    </Screen>
+    <NavBar/>
+
+    
+      
+     
+
+    
      
       
       
-    </Screen>
+    
    
    
-    <NavBar/>
     </>
    
     
