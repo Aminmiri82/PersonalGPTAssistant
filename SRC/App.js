d1943cac7da40134d579ac4dfde30fbf1d197f34
Ali <aliradmard5@gmail.com>
2024-02-23 22:33:20 +0330

    DO NOT INSTALL UNSAFE DEPENDANCEIS

--- SRC/App.js
+++ SRC/App.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
 
-import { StyleSheet, Text, View } from "react-native";
+import Screen from "./Components/Screen";
+import NavBar from "./Components/NavBar";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Header from "./Components/Header";
@@ -12,15 +12,8 @@ import AssistantsMenuItem from "./Components/AssistantsMenuItem";
 import ListItem from "./Components/ListItem";
 import ListItemDeleteAction from "./Components/ListItemDeleteAction";
 import OnBoardingScreen from "./Screens/OBS/OnBoardingScreen";
-import LinearGradientScreen from "./Screens/LinearGradinetScreen";
-import testOBS from "./Screens/OBS/testOBS";
-
 export default function App() {
-  return (
-    // <NavigationContainer>
-
-    // </NavigationContainer>
-
-    <OnBoardingScreen />
-  );
+  return <OnBoardingScreen />;
 }
+
+const styles = StyleSheet.create({});
