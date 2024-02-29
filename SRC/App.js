85bcfb395bc0cabfa6da605bffce18578e6ee102
Ali <aliradmard5@gmail.com>
2024-02-29 22:07:50 +0330

    Revert "created empty Assistant screen"
    This reverts commit e6158c57ebdb89be7ad8ee5e41170b5a241238ac.

--- SRC/App.js
+++ SRC/App.js
@@ -7,13 +7,21 @@ import Screen from "./Components/Screen";
 import ListItem from "./Components/ListItem";
 import ListItemDeleteAction from "./Components/ListItemDeleteAction";
 
+
 import TestScreen from "./Screens/TestScreen";
 import AboutUsScreen from "./Screens/SettingsScreen/AboutUsScreen";
 import OnBoardingScreen from "./Screens/OBS/OnBoardingScreen";
-import BuildScreen from "./Screens/AssistantScreen/BuildScreen";
 
 export default function App() {
-  return <BuildScreen />;
+  return (
+    <>
+      
+      <Screen>
+        <OnBoardingScreen back="back" next="next"/>
+      </Screen>
+      
+    </>
+  );
 }
 
 const styles = StyleSheet.create({});
