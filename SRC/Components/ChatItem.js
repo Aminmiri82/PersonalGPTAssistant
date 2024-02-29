713738071fe3eb33af34bf46cb335549e5152499
Ali <aliradmard5@gmail.com>
2024-02-29 22:09:29 +0330

    Fukced up more

---
+++ SRC/Components/ChatItem.js
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TouchableHighlight,
+} from "react-native";
+import AppText from "./AppText";
+import colors from "../config/colors";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+
+function ListItem({
+  title,
+  subTitle,
+  image,
+  IconComponent,
+  onPress,
+  renderRightActions,
+}) {
+  return (
+    <Swipeable renderRightActions={renderRightActions}>
+      <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+        <View style={styles.container}>
+          {IconComponent}
+          {image && <Image style={styles.image} source={image} />}
+          <View style={styles.detailContainer}>
+            <AppText style={styles.title}>{title}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+          </View>
+        </View>
+      </TouchableHighlight>
+    </Swipeable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    padding: 10,
+    backgroundColor: colors.white,
+  },
+  detailContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    marginRight: 10,
+  },
+  title: {
+    fontWeight: "500",
+  },
+  subTitle: {
+    color: colors.medium,
+  },
+});
+export default ListItem;
