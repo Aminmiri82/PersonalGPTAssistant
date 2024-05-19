f8ea7de3a62e46517758c2a310b280261409750d
Aminmiri82 <miri.amin@ymail.com>
2024-05-19 22:36:01 +0200

    added settingsScreen

--- SRC/App.js
+++ SRC/App.js
@@ -6,6 +6,9 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SettingsScreen from "./Screens/SettingsScreen/SettingsScreen";
 import NavBar from "./Components/NavBar";
 import LanguagesPrompt from "./Components/SettingsComponents/LanguagesPrompt";
+import AboutUsScreen from "./Screens/SettingsScreen/AboutUsScreen";
+import TermsAndConditionsScreen from "./Screens/SettingsScreen/TermsAndConditionsScreen";
+import PrivacyPolicyScreen from "./Screens/SettingsScreen/PrivacyPolicyScreen";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -14,6 +17,9 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="TermsAndConditionsScreen" component={TermsAndConditionsScreen} />
+        <Stack.Screen name="PrivacyPolicyScreen" component={PrivacyPolicyScreen} />
+        <Stack.Screen name="AboutUsScreen" component={AboutUsScreen} />
         
         
       </Stack.Navigator>
--- SRC/Screens/SettingsScreen/SettingsScreen.js
+++ SRC/Screens/SettingsScreen/SettingsScreen.js
@@ -5,58 +5,80 @@ import LanguagesPrompt from "../../Components/SettingsComponents/LanguagesPrompt
 import OpenAIPrompt from "../../Components/SettingsComponents/OpenAIPrompt";
 import SettingsItem from "../../Components/SettingsComponents/SettingsItem";
 import Icon from "../../Components/Icon";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+
+
 import { useState } from "react";
 
-function SettingsScreen(props) {
-    const [isLanguagePromptVisible, setLanguagePromptVisible] = useState(false);
-    const [isAPIPromptVisible, setAPIPromptVisible] = useState(false);
-  
-    const [selectedLanguage, setSelectedLanguage] = useState("Select Language");
-    const [apiKey, setApiKey] = useState("Enter API Key");
-  
-    const toggleLanguagePrompt = () => {
-      setLanguagePromptVisible(!isLanguagePromptVisible);
-    };
-  
-    const toggleAPIPrompt = () => {
-      setAPIPromptVisible(!isAPIPromptVisible);
-    };
-  
-    const handleSelectLanguage = (language) => {
-      setSelectedLanguage(language);
-      setLanguagePromptVisible(false);
-    };
-  
-    const handleSetAPIKey = (key) => {
-      setApiKey(key);
-      setAPIPromptVisible(false);
-    };
+function SettingsScreen({navigation, route}) {
+  const [isLanguagePromptVisible, setLanguagePromptVisible] = useState(false);
+  const [isAPIPromptVisible, setAPIPromptVisible] = useState(false);
+
+  const [selectedLanguage, setSelectedLanguage] = useState("Select Language");
+  const [apiKey, setApiKey] = useState("Enter API Key");
+
+  const toggleLanguagePrompt = () => {
+    setLanguagePromptVisible(!isLanguagePromptVisible);
+  };
+
+  const toggleAPIPrompt = () => {
+    setAPIPromptVisible(!isAPIPromptVisible);
+  };
+
+  const handleSelectLanguage = (language) => {
+    setSelectedLanguage(language);
+    setLanguagePromptVisible(false);
+  };
+
+  const handleSetAPIKey = (key) => {
+    setApiKey(key);
+    setAPIPromptVisible(false);
+  };
 
   return (
-    <View style={styles.container}>
-      <SettingsItem
-        title="OpenAI API Key"
-        subTitle={apiKey}
-        IconComponent={<Icon iconSet="MCI" name="key"/>}
-        onPress={toggleAPIPrompt}
+    <>
+      <View style={styles.container}>
+        <SettingsItem
+          title="OpenAI API Key"
+          subTitle={apiKey}
+          IconComponent={<Icon iconSet="MCI" name="key" />}
+          onPress={toggleAPIPrompt}
         />
         <OpenAIPrompt
-            visible={isAPIPromptVisible}
-            onClose={toggleAPIPrompt}
-            onSumbit={handleSetAPIKey}
+          visible={isAPIPromptVisible}
+          onClose={toggleAPIPrompt}
+          onSumbit={handleSetAPIKey}
         />
-      <SettingsItem
-        title="Languages"
-        subTitle={selectedLanguage}
-        IconComponent={<Icon iconSet="MCI" name="translate"/>}
-        onPress={toggleLanguagePrompt}
-      />
-      <LanguagesPrompt
-        visible={isLanguagePromptVisible}
-        onClose={toggleLanguagePrompt}
-        onSelectLanguage={handleSelectLanguage}
-      />
-    </View>
+        <SettingsItem
+          title="Languages"
+          subTitle={selectedLanguage}
+          IconComponent={<Icon iconSet="MCI" name="translate" />}
+          onPress={toggleLanguagePrompt}
+        />
+        <LanguagesPrompt
+          visible={isLanguagePromptVisible}
+          onClose={toggleLanguagePrompt}
+          onSelectLanguage={handleSelectLanguage}
+        />
+        <SettingsItem
+          title="Terms and Conditions"
+          IconComponent={<Icon iconSet="MCI" name="file-document" />}
+          onPress={() => navigation.navigate("TermsAndConditionsScreen")}
+        />
+        <SettingsItem
+          title="Privacy Policy"
+          IconComponent={<Icon iconSet="MCI" name="file-document" />}
+          onPress={() => navigation.navigate("PrivacyPolicyScreen")}
+        />
+        <SettingsItem
+          title="About Us"
+          IconComponent={<Icon iconSet="MCI" name="information" />}
+          onPress={() => navigation.navigate("AboutUsScreen")}
+        />
+      </View>
+    </>
   );
 }
 
