07ef10711808829ce9f720baee5e36f1431ce3ca
Aminmiri82 <miri.amin@ymail.com>
2024-05-19 20:50:58 +0200

    added languages prompt

--- SRC/Components/SettingsComponents/LanguagesPrompt.js
+++ SRC/Components/SettingsComponents/LanguagesPrompt.js
@@ -1,41 +1,77 @@
 import React from "react";
-import { View, StyleSheet, Alert, Button } from "react-native";
+import { View, StyleSheet, Modal, TouchableOpacity } from "react-native";
 
-function LanguagesPrompt(props) {
-  const createTwoButtonAlert = () =>
-    Alert.alert("choose the apps language", message, [
-      {
-        text: "Cancel",
-        onPress: () => console.log("Cancel Pressed"),
-        style: "cancel",
-      },
-      { text: "OK", onPress: () => console.log("OK Pressed") },
-    ]);
+import AppText from "../AppText";
 
-  const apiKeyAlert = () =>
-    Alert.prompt("Enter API Key", "Enter your OpenAI API Key", [
-      {
-        text: "Cancel",
-        onPress: () => console.log("Cancel Pressed"),
-        style: "cancel",
-      },
-      {
-        text: "OK",
-        onPress: (apiKey) => console.log("OK Pressed, API Key: " + apiKey),
-        style: "default",
-        AlertType: "secure-text",
-      },
-    ]);
+function LanguagesPrompt({ visible, onClose, onSelectLanguage }) {
   return (
-    <View style={styles.container}>
-      <Button title={"2-Button Alert"} onPress={createTwoButtonAlert} />
-      <Button title={"API Key Alert"} onPress={apiKeyAlert} />
-    </View>
+    <Modal
+      transparent={true}
+      animationType="slide"
+      visible={visible}
+      onRequestClose={onClose}
+    >
+      <View style={styles.modalBackground}>
+        <View style={styles.menuContainer}>
+          <TouchableOpacity
+            style={styles.menuItem}
+            onPress={() => {
+              onSelectLanguage("English");
+            }}
+          >
+            <AppText style={styles.menuItemAppText}>Java</AppText>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.menuItem}
+            onPress={() => {
+              onSelectLanguage("Persian");
+            }}
+          >
+            <AppText style={styles.menuItemAppText}>JavaScript</AppText>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.menuItem}
+            onPress={() => {
+              onSelectLanguage("French");
+            }}
+          >
+            <AppText style={styles.menuItemAppText}>Python</AppText>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem} onPress={onClose}>
+            <AppText style={styles.menuItemAppText}>Cancel</AppText>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  modalBackground: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  menuContainer: {
+    width: 300,
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  menuItem: {
+    padding: 15,
+  },
+  menuItemAppText: {
+    fontSize: 18,
+  },
 });
-
 export default LanguagesPrompt;
