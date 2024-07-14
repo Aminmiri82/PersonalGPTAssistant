379b602f50f185339a787313236510cd98e97089
Ali <aliradmard5@gmail.com>
2024-07-14 15:14:12 +0330

    fixed the empty chat bubble bug and merges textinput and chattextinput

--- SRC/Navigation/ChatScreenNav.js
+++ SRC/Navigation/ChatScreenNav.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import ChatMenuScreen from "../Screens/ChatScreen/ChatMenuScreen";
+import ChatScreen from "../Screens/ChatScreen/ChatScreen";
 import OnBoardingScreen from "../Screens/OBS/OnBoardingScreen";
 import AppButton from "../Components/AppButton";
 import Icon from "../Components/Icon";
@@ -9,19 +9,17 @@ import Icon from "../Components/Icon";
 const ChatStack = createNativeStackNavigator();
 
 const makeNewChatButton = (navigation) => (
-
   <AppButton
     title="new chat"
     onPress={() => navigation.navigate("OnBoardingScreen")}
   />
-  
 );
 function ChatScreenNav(props) {
   return (
     <ChatStack.Navigator>
       <ChatStack.Screen
         name="ChatMenuScreen"
-        component={ChatMenuScreen}
+        component={ChatScreen}
         options={({ navigation }) => ({
           headerRight: () => makeNewChatButton(navigation),
         })}
--- SRC/Screens/ChatScreen/ChatMenuScreen.js
+++ SRC/Screens/ChatScreen/ChatMenuScreen.js
@@ -1,84 +1,14 @@
 import React from "react";
-import { StyleSheet, View, FlatList, TextInput } from "react-native";
-
+import { View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 import ChatItem from "../../Components/ChatComponents/ChatItem";
-import Screen from "../../Components/Screen";
 
-import colors from "../../config/colors";
-import Icon from "../../Components/Icon";
-import ListItemSeparator from "../../Components/ListItemSeparator";
-import Textinput from "../../Components/ChatComponents/Textinput";
-// note from amin : what the fuck is this shit? i thought this was supposed to be the chatmenuScreen wtf
-const menuItems = [
-  {
-    title: "My listings",
-    icon: {
-      name: "format-list-bulleted",
-      backgroundColor: colors.primary,
-    },
-  },
-  {
-    title: "My message",
-    icon: {
-      name: "email",
-      backgroundColor: colors.secondary,
-    },
-  },
-];
-
-function ChatMenuScreen({ navigation }) {
+function ChatMenuScreen() {
   return (
-    <Screen style={styles.screen}>
-      <View style={styles.textinputcontainer}>
-        <Textinput style={styles.textinput} />
-      </View>
-      {/* <View style={styles.container}>
-        <ChatItem
-          title="place holder"
-          subTitle="aliradmard5@gmail.com"
-          image={require("../../assets/mosh.jpg")}
-        />
-      </View>
-      <View style={styles.container}>
-        <FlatList
-          data={menuItems}
-          keyExtractor={(menuItem) => menuItem.title}
-          ItemSeparatorComponent={ListItemSeparator}
-          renderItem={({ item }) => (
-            <ChatItem
-              title={item.title}
-              IconComponent={
-                <Icon
-                  iconSet="MCI" //shortened for MaterialCommunityIcons
-                  name={item.icon.name}
-                  backgroundColor={item.icon.backgroundColor} //FIXME - backgroundColor in Icon
-                />
-              }
-            />
-          )}
-        />
-      </View>
-      <ChatItem
-        title="Logout"
-        IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
-        onPress={() => navigation.navigate("OnBoardingScreen")} // temp for making sure the navigation thingy works
-      /> */}
-    </Screen>
+    <View>
+      <ChatItem title="Hello" subTitle="Hi" />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  screen: {
-    backgroundColor: colors.light,
-  },
-  textinputcontainer: {
-    position: "absolute",
-    bottom: "5%",
-    left: 0,
-    right: 0,
-    backgroundColor: colors.light,
-    borderColor: "red",
-  },
-});
 
 export default ChatMenuScreen;
