7dadd3349932e65bbdbe1e54edfacda8de080a8f
Ali <aliradmard5@gmail.com>
2024-07-17 13:15:35 +0330

    finished chatItem, working of navigation

---
+++ SRC/Navigation/ChatMenuScreenNav.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+function ChatMenuScreenNav({ navigation }) {
+  return <View></View>;
+}
+
+export default ChatMenuScreenNav;
--- SRC/Screens/ChatScreen/ChatMenuScreen.js
+++ SRC/Screens/ChatScreen/ChatMenuScreen.js
@@ -2,11 +2,17 @@ import React from "react";
 import { View } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import ChatItem from "../../Components/ChatComponents/ChatItem";
+import ChatScreen from "./ChatScreen";
 
-function ChatMenuScreen() {
+function ChatMenuScreen({ navigation }) {
   return (
     <View>
-      <ChatItem title="Hello" subTitle="Hi" />
+      <ChatItem
+        title="auto generated name"
+        subTitle="last message in thread"
+        image="../../assets/IMG_1706.jpeg"
+        modelname="gpt"
+      />
     </View>
   );
 }
