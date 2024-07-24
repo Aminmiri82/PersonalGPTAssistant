67504884e5b24334f9aa328319f8e0bdeec1ee86
Aminmiri82 <miri.amin@ymail.com>
2024-07-24 12:57:02 +0200

    made first api call

--- SRC/Screens/ChatScreen/ChatScreen.js
+++ SRC/Screens/ChatScreen/ChatScreen.js
@@ -1,8 +1,25 @@
 import React, { useState } from "react";
 import { View, Button, TextInput } from "react-native";
 import { getDB } from "../../database";
+import { OPENAI_API_KEY } from "@env";
+
+import OpenAI from "openai";
 
 const ChatScreen = ({ navigation }) => {
+  const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+
+  async function call() {
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: "What is a LLM?" },
+      ],
+      model: "gpt-4o-mini",
+    });
+
+    console.log(completion.choices[0]);
+  }
+  
   const [newChatTitle, setNewChatTitle] = useState("");
 
   const addChatItem = () => {
@@ -31,7 +48,7 @@ const ChatScreen = ({ navigation }) => {
         value={newChatTitle}
         onChangeText={setNewChatTitle}
       />
-      <Button title="Add Chat" onPress={addChatItem} />
+      <Button title="Add Chat" onPress={call} />
     </View>
   );
 };
