b9d83a5ffd0af58b7a3db6cd62ede0b7a1f13df9
Aminmiri82 <miri.amin@ymail.com>
2024-07-24 12:06:07 +0200

    Revert "added .env"
    This reverts commit 8d643decccab14d54c84cf22b5c4ffe0ad71a6d2.

--- SRC/Screens/ChatScreen/ChatMenuScreen.js
+++ SRC/Screens/ChatScreen/ChatMenuScreen.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { View, FlatList, Text,Button } from "react-native";
+import { View, FlatList, Text } from "react-native";
 import { initDB, getDB } from "../../database";
 import ChatItem from "../../Components/ChatComponents/ChatItem";
 
-const ChatMenuScreen = ({ navigation }) => {
+const ChatMenuScreen = () => {
   const [chatItems, setChatItems] = useState([]);
 
   useEffect(() => {
@@ -25,10 +25,6 @@ const ChatMenuScreen = ({ navigation }) => {
   return (
     <View>
       <Text>first commit</Text>
-      <Button
-        title="New Chat"
-        onPress={() => navigation.navigate("ChatScreen")}
-      />
       <FlatList
         data={chatItems}
         keyExtractor={(item) => item.id.toString()}
