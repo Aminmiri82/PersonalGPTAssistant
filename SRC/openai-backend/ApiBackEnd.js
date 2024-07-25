a651aa815a2e7c3ed3531da0c4031529da8ad75a
Aminmiri82 <miri.amin@ymail.com>
2024-07-25 20:31:54 +0200

    deleted server.js

---
+++ SRC/openai-backend/ApiBackEnd.js
@@ -0,0 +1,47 @@
+// assistantApi.js
+import OpenAI from "openai";
+import { OPENAI_API_KEY } from "@env";
+
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+
+const callAssistantApi = async (message) => {
+  try {
+    console.log("Creating thread...");
+    const thread = await openai.beta.threads.create();
+    console.log("Thread created:", thread.id);
+
+    console.log("Sending message to thread...");
+    await openai.beta.threads.messages.create(thread.id, {
+      role: "user",
+      content: message,
+    });
+
+    console.log("Message sent to thread:", message);
+
+    console.log("Running assistant...");
+    const run = await openai.beta.threads.runs.createAndPoll(thread.id, {
+      assistant_id: "asst_40ROFN9nKe2V6Eka6bYXSZ2y", // Use your assistant ID
+    });
+
+    if (run.status === "completed") {
+      console.log("Assistant run completed");
+      const messages = await openai.beta.threads.messages.list(run.thread_id);
+      console.log("Messages received from thread:", messages.data);
+      const assistantMessage = messages.data[0].content[0].text.value;
+
+      if (assistantMessage) {
+        console.log("Assistant message:", assistantMessage);
+        return assistantMessage;
+      } else {
+        throw new Error("No assistant message found in the response");
+      }
+    } else {
+      throw new Error("Assistant run failed");
+    }
+  } catch (error) {
+    console.error("Error calling assistant:", error);
+    throw error;
+  }
+};
+
+export default callAssistantApi;
