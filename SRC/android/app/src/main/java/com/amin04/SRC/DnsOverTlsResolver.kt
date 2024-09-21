954b9985f5f01489e7ecee42923ee93713b98f19
Ali <aliradmard5@gmail.com>
2024-09-21 19:51:26 +0330

    probably fixed now

--- SRC/android/app/src/main/java/com/amin04/SRC/DnsOverTlsResolver.kt
+++ SRC/android/app/src/main/java/com/amin04/SRC/DnsOverTlsResolver.kt
@@ -1,17 +1,19 @@
 package com.amin04.SRC
 
 import okhttp3.Dns
+import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.dnsoverhttps.DnsOverHttps
 import java.net.InetAddress
 
 class DnsOverTlsResolver {
+
     fun createDnsOverHttpsClient(): OkHttpClient {
         // Use Google's DoT server as an example
         val dns = DnsOverHttps.Builder()
             .client(OkHttpClient())
-            .url("https://free.shecan.ir/dns-query")
+            .url(HttpUrl.get("free.shecan.ir"))  // Fix: Convert string to HttpUrl
             .includeIPv6(true)
             .build()
 
